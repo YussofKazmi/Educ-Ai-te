49ac3eb0ff13ddd5c3004f36e3a33826882088b4
JFM-Yussof <yussofk@jfmx3x.com>
2025-12-03 12:19:33 -0500

    added the agents and tasks to the crew.py file

--- educaite/src/educaite/crew.py
+++ educaite/src/educaite/crew.py
@@ -13,6 +13,9 @@ class Educaite():
     agents: List[BaseAgent]
     tasks: List[Task]
 
+    agents_config = "config/agents.yaml"
+    tasks_config = "config/tasks.yaml"
+
     # Learn more about YAML configuration files here:
     # Agents: https://docs.crewai.com/concepts/agents#yaml-configuration-recommended
     # Tasks: https://docs.crewai.com/concepts/tasks#yaml-configuration-recommended
@@ -22,14 +25,38 @@ class Educaite():
     @agent
     def researcher(self) -> Agent:
         return Agent(
-            config=self.agents_config['researcher'], # type: ignore[index]
+            config=self.agents_config['research_agent'], # type: ignore[index]
             verbose=True
         )
 
     @agent
-    def reporting_analyst(self) -> Agent:
+    def knowledge_compiler(self) -> Agent:
         return Agent(
-            config=self.agents_config['reporting_analyst'], # type: ignore[index]
+            config=self.agents_config['knowledge_agent'], # type: ignore[index]
+            verbose=True
+        )
+
+
+
+    @agent
+    def educator(self) -> Agent:
+        return Agent(
+            config=self.agents_config['educator_agent'], # type: ignore[index]
+            verbose=True
+        )
+        
+        
+    @agent
+    def quiz_creator(self) -> Agent:
+        return Agent(
+            config=self.agents_config['quiz_agent'], # type: ignore[index]
+            verbose=True
+        )
+        
+    @agent
+    def result_delivery(self) -> Agent:
+        return Agent(
+            config=self.agents_config['result_delivery_agent'], # type: ignore[index]
             verbose=True
         )
 
@@ -39,13 +66,34 @@ class Educaite():
     @task
     def research_task(self) -> Task:
         return Task(
-            config=self.tasks_config['research_task'], # type: ignore[index]
+            config=self.tasks_config['gather_information_task'], # type: ignore[index]
         )
 
     @task
-    def reporting_task(self) -> Task:
+    def compile_knowledge(self) -> Task:
         return Task(
-            config=self.tasks_config['reporting_task'], # type: ignore[index]
+            config=self.tasks_config['compile_knowledge_task'], # type: ignore[index]
+            output_file='report.md'
+        )
+
+    @task
+    def create_guide(self) -> Task:
+        return Task(
+            config=self.tasks_config['create_guide_task'], # type: ignore[index]
+            #output_file='report.md'
+        )
+
+    @task
+    def generate_quiz(self) -> Task:
+        return Task(
+            config=self.tasks_config['generate_quiz_task'], # type: ignore[index]
+            #output_file='report.md'
+        )
+
+    @task
+    def assemble_result(self) -> Task:
+        return Task(
+            config=self.tasks_config['assemble_result_task'], # type: ignore[index]
             output_file='report.md'
         )
 
